630a80dd1d256853cd383eb3f89aec15147d7d68
JBTF <josetorresfuentes90@gmail.com>
2021-03-03 13:30:10 +0000

    Project_SAPUI5

---
+++ Invoices/SAPUI5/webapp/Component.js
@@ -0,0 +1,42 @@
+// @ts-nocheck
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "logaligroup/SAPUI5/model/Models",
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog"
+],
+    /**
+     * @param {typeof sap.ui.core.UIComponent} UIComponent
+     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
+     */
+    function (UIComponent, Models, ResourceModel, HelloDialog) {
+
+        return UIComponent.extend("logaligroup.SAPUI5.Component", {
+
+            metadata: {
+                manifest : "json"
+            },
+
+            init: function () {
+
+                UIComponent.prototype.init.apply(this, arguments);
+
+                this.setModel(Models.createRecipient());
+
+                var i18nModel = new ResourceModel({ bundleName: "logaligroup.SAPUI5.i18n.i18n" });
+                this.setModel(i18nModel, "i18n");
+
+                this._helloDialog = new HelloDialog(this.getRootControl());
+            },
+
+            exit: function () {
+                this._helloDialog.destroy();
+                delete this._helloDialog;
+            },
+
+            openHelloDialog: function () {
+                this._helloDialog.open();
+
+            }
+        });
+    });
